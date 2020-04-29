e96605285bea47a6cb37db42c9063b0d6e766d1d
Evan Gubarev <evan@evangubarev.com>
2020-04-28 21:47:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xsquared',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['.github*']),
     license='MIT',
     description='a python package that squares its imput',
